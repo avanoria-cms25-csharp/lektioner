80bbb9f015d923986ce6fed2f5cd4cddd96db1f8
Hans Mattin-Lassei <hans.mattin-lassei@avanoria.se>
2025-09-12 10:04:25 +0200

    20250912

---
+++ 20250912/20250912/Infrastructure/Models/ProductUpdateRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.Models;
+
+public class ProductUpdateRequest
+{
+    public string? ArticleNumber { get; set; }
+    public bool IsService { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+}
