83a408589d23a4130b7f32ec2a9e3f9fc15be84e
Hans Mattin-Lassei <hans.mattin-lassei@avanoria.se>
2025-09-23 14:59:19 +0200

    20250923

---
+++ 20250923/WPF/MainApp/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MainApp;
+
+public partial class MainWindow : Window
+{
+    private CancellationTokenSource _cts = new();
+    private bool _isDownloading = false;
+
+    private static readonly string _fileName = @"2025-05-13-raspios-bookworm-arm64-lite.img.xz";
+    private static readonly string _sourceFilePath = @$"https://downloads.raspberrypi.com/raspios_lite_arm64/images/raspios_lite_arm64-2025-05-13/{_fileName}";
+    private static readonly string _targetFilePath = @$"c:\downloads\{_fileName}";
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+    }
+
+    private async Task DownloadAsync(CancellationToken cancellationToken)
+    {
+        _isDownloading = true;
+
+        using var http = new HttpClient();
+        var res = await http.GetAsync(_sourceFilePath, cancellationToken);
+
+        if (res.IsSuccessStatusCode)
+        {
+            using var fileStream = new FileStream(_targetFilePath, FileMode.CreateNew);
+            await res.Content.CopyToAsync(fileStream, cancellationToken);
+        }
+    }
+
+
+    private async void Btn_Download_Click(object sender, RoutedEventArgs e)
+    {
+        if (_isDownloading)
+        {
+            _cts.Cancel();
+            return;
+        }
+
+        try
+        {
+            Lview_EventLog.Items.Add($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} : Start downloading...");
+
+            _cts = new CancellationTokenSource();
+            Btn_Download.Content = "Cancel";
+
+            await DownloadAsync(_cts.Token);
+
+            Lview_EventLog.Items.Add($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} : Download completed.");
+        }
+        catch (Exception ex)
+        {
+            Lview_EventLog.Items.Add($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} : {ex.Message}");
+        }
+        finally
+        {
+            _isDownloading = false;
+            Btn_Download.Content = "Download";
+            _cts = null!;
+        }
+
+    }
+}
---
+++ 20250923/WindowsForms/MainApp/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace MainApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            BtnSubmit = new Button();
+            label1 = new Label();
+            Input_FullName = new TextBox();
+            SuspendLayout();
+            // 
+            // BtnSubmit
+            // 
+            BtnSubmit.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            BtnSubmit.Location = new Point(201, 227);
+            BtnSubmit.Name = "BtnSubmit";
+            BtnSubmit.Size = new Size(280, 76);
+            BtnSubmit.TabIndex = 0;
+            BtnSubmit.Text = "Submit";
+            BtnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(152, 91);
+            label1.Name = "label1";
+            label1.Size = new Size(107, 30);
+            label1.TabIndex = 1;
+            label1.Text = "Full Name";
+            // 
+            // Input_FullName
+            // 
+            Input_FullName.Location = new Point(152, 124);
+            Input_FullName.Name = "Input_FullName";
+            Input_FullName.Size = new Size(417, 35);
+            Input_FullName.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(698, 395);
+            Controls.Add(Input_FullName);
+            Controls.Add(label1);
+            Controls.Add(BtnSubmit);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button BtnSubmit;
+        private Label label1;
+        private TextBox Input_FullName;
+    }
+}
