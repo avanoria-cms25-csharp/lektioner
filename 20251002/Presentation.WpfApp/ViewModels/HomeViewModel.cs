90ce2e3648c5585724b93da372ae9445998e3fb4
Hans Mattin-Lassei <hans.mattin-lassei@avanoria.se>
2025-10-02 15:26:21 +0200

    20251002

--- 20251002/Presentation.WpfApp/ViewModels/HomeViewModel.cs
+++ 20251002/Presentation.WpfApp/ViewModels/HomeViewModel.cs
@@ -1,7 +1,23 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Presentation.WpfApp.ViewModels;
 
 public partial class HomeViewModel : ObservableObject
 {
+    [ObservableProperty]
+    private bool _isFanOn;
+
+    [ObservableProperty]
+    private string _buttonText = "Turn On";
+
+    [ObservableProperty]
+    private double _speedRatio = 1.0;
+
+    [RelayCommand]
+    private void ToggleFanState()
+    {
+        IsFanOn = !IsFanOn;
+        ButtonText = IsFanOn ? "Turn Off" : "Turn On";
+    }
 }
