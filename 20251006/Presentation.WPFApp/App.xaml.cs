bf733f2c5a5f7b828af81cc577d77901347499c7
Hans Mattin-Lassei <hans.mattin-lassei@avanoria.se>
2025-10-06 09:20:28 +0200

    20251006

--- 20251006/Presentation.WPFApp/App.xaml.cs
+++ 20251006/Presentation.WPFApp/App.xaml.cs
@@ -1,7 +1,31 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Presentation.WPFApp.ViewModels;
+using System.Windows;
 
 namespace Presentation.WPFApp;
 
 public partial class App : Application
 {
+    private IHost? _host;
+
+    public App()
+    {
+        _host = Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddSingleton<MainViewModel>();
+                services.AddSingleton<MainWindow>();
+            })
+            .Build();
+    }
+
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var window = _host!.Services.GetRequiredService<MainWindow>();
+        window.Show();
+    }
 }
