e39c42b28210eab8d5289ac73d09ebbafd446dcd
Hans Mattin-Lassei <hans.mattin-lassei@avanoria.se>
2025-10-06 10:45:14 +0200

    20251006

--- 20251006/Presentation.WPFApp/ViewModels/UserListViewModel.cs
+++ 20251006/Presentation.WPFApp/ViewModels/UserListViewModel.cs
@@ -1,15 +1,25 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Infrastructure.Models;
+using Infrastructure.Services;
 using Microsoft.Extensions.DependencyInjection;
 using System.Collections.ObjectModel;
 
 namespace Presentation.WPFApp.ViewModels;
 
-public partial class UserListViewModel(IServiceProvider serviceProvider) : ObservableObject
+public partial class UserListViewModel : ObservableObject
 {
-    private readonly IServiceProvider _serviceProvider = serviceProvider;
-    
+    private readonly IServiceProvider _serviceProvider;
+    private readonly IUserService _userService;
+
+    public UserListViewModel(IServiceProvider serviceProvider, IUserService userService)
+    {
+        _serviceProvider = serviceProvider;
+        _userService = userService;
+
+        PopulateUserList();
+    }
+
     [ObservableProperty]
     private string _title = "USER LIST";
 
@@ -21,5 +31,12 @@ public partial class UserListViewModel(IServiceProvider serviceProvider) : Obser
     {
         var mmv = _serviceProvider.GetRequiredService<MainViewModel>();
         mmv.CurrentViewModel = _serviceProvider.GetRequiredService<UserAddViewModel>();
+    }
+
+
+    public void PopulateUserList()
+    {
+        var currentUsers = _userService.GetUsers();
+        Users = new ObservableCollection<User>(currentUsers);
     }
 }
